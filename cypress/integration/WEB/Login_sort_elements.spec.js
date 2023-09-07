ef2d1a0a7d304655285adbe4e6fd8fc0170a143c
Heitor-Rosani <heitor.rsn@hotmail.com>
2023-09-07 16:50:26 -0300

    Inicial commit

--- cypress/integration/WEB/Login_sort_elements.spec.js
+++ cypress/integration/WEB/Login_sort_elements.spec.js
@@ -4,11 +4,11 @@ describe('Analising the site', () => {
   beforeEach(()=>{
     cy.login()
   })
-  it('Sorting A-Z', ()=>{
+  it.only('Sorting A-Z', ()=>{
     cy.get('[data-test="product_sort_container"]').select('Name (A to Z)')
     let item_list = [] 
     cy.create_list('.inventory_item_name', item_list)
-    cy.log(item_list)
+    cy.log(item_list[0])
  })
 
  it('Sorting Z-A', () => {
